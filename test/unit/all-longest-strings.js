ddf208641f2ce98819575a378b09deb560d710be
Michał Czyż <m.czyz@selleo.com>
2017-07-27 16:22:18 +0200

    feedback to tests - ensure the verification is strict enough

--- test/unit/all-longest-strings.js
+++ test/unit/all-longest-strings.js
@@ -1,19 +1,12 @@
 import allLongestStrings from '../../src/all-longest-strings';
 
 describe('allLongestStrings()', () => {
-
-    it('should retun an array', () => {
-        let inputData = ['xzc', 'sadf', 'qwerty', 'ytrewq', 'sdfgh'];
-        
-        expect(allLongestStrings(inputData)).to.be.a('array');
-    });
-
     it('should return an array containing all longest strings', () => {
         let inputData = ['xzc', 'sadf', 'qwerty', 'ytrewq', 'sdfgh'];
+        let output = allLongestStrings(inputData);
 
-        expect(allLongestStrings(inputData)).to.include('qwerty', 'yrtewq');
-        expect(allLongestStrings(inputData)[0]).to.eq('qwerty');
-        expect(allLongestStrings(inputData)[1]).to.eq('ytrewq');
+        expect(output).to.include('qwerty', 'ytrewq');
+        expect(output.length).to.eq(2);
     });
 
     it('should return one element as it is', () => {
