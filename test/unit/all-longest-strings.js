2db7f3c8f56b09558ec6d2a758fde56f324f28a0
Alan Pieczonka <alanpieczonka@gmail.com>
2017-07-31 21:52:03 +0200

    All-longest-strings tests improvement

--- test/unit/all-longest-strings.js
+++ test/unit/all-longest-strings.js
@@ -5,21 +5,26 @@ describe('allLongestStrings()', () => {
     it('should retun an array', () => {
         let inputData = ['xzc', 'sadf', 'qwerty', 'ytrewq', 'sdfgh'];
         
-        expect(allLongestStrings(inputData)).to.be.a('array');
+        expect(allLongestStrings(inputData)).to.be.an('array');
     });
 
-    it('should return an array containing all longest strings', () => {
-        let inputData = ['xzc', 'sadf', 'qwerty', 'ytrewq', 'sdfgh'];
+    context('for more than one element in an array', () => {
+        it('should return an array containing all longest strings', () => {
+            let inputData1 = ['xzc', 'sadf', 'qwerty', 'ytrewq', 'sdfgh'],
+                inputData2 = ['neutra', 'Fingerstache', 'Mumblecore'];
 
-        expect(allLongestStrings(inputData)).to.include('qwerty', 'yrtewq');
-        expect(allLongestStrings(inputData)[0]).to.eq('qwerty');
-        expect(allLongestStrings(inputData)[1]).to.eq('ytrewq');
+            expect(allLongestStrings(inputData1)).to.include('qwerty', 'ytrewq');
+            expect(allLongestStrings(inputData2)).to.include('Fingerstache');
+        });
     });
 
-    it('should return one element as it is', () => {
-        let inputData = ['aa'];
-        expect(allLongestStrings(inputData)).to.include('aa');
+    context('for one element in an array', () =>{
+        it('should return element as it is', () => {
+            let inputData1 = ['aa'],
+            inputData2 = ['zxcvbnm'];
+            expect(allLongestStrings(inputData1)).to.include('aa');
+            expect(allLongestStrings(inputData2)).to.include('zxcvbnm');
+        });
     });
-
 
 });
