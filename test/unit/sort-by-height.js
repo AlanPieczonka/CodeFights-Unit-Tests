61f76e6e72e0f134167cc2a1e8f7b67f4fbbd2d3
Alan Pieczonka <alanpieczonka@gmail.com>
2017-08-01 23:33:14 +0200

    Sort-by-height tests improvement

--- test/unit/sort-by-height.js
+++ test/unit/sort-by-height.js
@@ -4,18 +4,36 @@ describe('sortByHeight()', () => {
 
     it('should return an array', () => {
         let inputData = [1,2];
-
-        expect(sortByHeight(inputData)).to.be.a('array');
+        expect(sortByHeight(inputData)).to.be.an('array');
     });
 
-    it('return sorted array as it is', () => {
+  context('for sorted arrays', () => {
+    it('should return array as it is', () => {
       let inputData = [1,2,3,4,5];
       
       expect(sortByHeight(inputData)).to.include(1,2,3,4,5);
       expect(sortByHeight(inputData)[2]).to.eq(3);
+      expect(sortByHeight(inputData)[4]).to.eq(5);
     })
+  });
 
-    it('sort data correctly', () => {
+  context('for arrays without -1 elements', () =>{
+    it('should return sorted array', () => {
+        let inputData1 = [124,36,1,12,365,45],
+            inputData2 = [3123,435,3455,123,6456,23];
+
+        expect(sortByHeight(inputData1)).to.include(1,12,36,45,124,365);
+        expect(sortByHeight(inputData1)[3]).to.eq(45);
+        expect(sortByHeight(inputData1)[5]).to.eq(365);
+
+        expect(sortByHeight(inputData2)).to.include(23,123,435,3123,435,6456);
+        expect(sortByHeight(inputData2)[0]).to.eq(23);
+        expect(sortByHeight(inputData2)[5]).to.eq(6456);
+    });
+  })
+
+  context('for arrays with -1 elements', () => {
+    it('should sort data correctly', () => {
       let inputData = [4,5,-1,3,2];
       
       expect(sortByHeight(inputData)).to.include(2,3,-1,4,5);
@@ -23,7 +41,7 @@ describe('sortByHeight()', () => {
       expect(sortByHeight(inputData)[0]).to.eq(2);
     })
 
-    it('return -1 elements as they are', () => {
+    it('should return -1 elements as they are', () => {
       let inputData = [-1, 160, 90, -1, -1, 30, 270, -1];
 
       expect(sortByHeight(inputData)).to.include(-1,30,90,-1,-1,160,270,-1);
@@ -31,5 +49,5 @@ describe('sortByHeight()', () => {
       expect(sortByHeight(inputData)[2]).to.eq(90);
       expect(sortByHeight(inputData)[7]).to.eq(-1);
     })
-
+  });
 });
