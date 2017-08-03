c9e5bfaf3dd937bc6a6f009a90738b61ccd05220
Fafruch <fafruszek@gmail.com>
2017-08-03 23:44:47 +0200

    Removed context, code cleaned-up using prettier, destructuring used in forEach

--- test/unit/alternating-sums.js
+++ test/unit/alternating-sums.js
@@ -1,28 +1,23 @@
-import alternatingSums from '../../src/alternating-sums'
+import alternatingSums from '../../src/alternating-sums';
 
 describe('alternatingSums()', () => {
-
-   context('always', () => {
-     it('should return an array', () => {
-       let input = [[32,54,123,43],[42,432]];
-       input.forEach((myArray) => {
-           expect(alternatingSums(myArray)).to.be.an('array');
-       });
-     });
-   });
+  it('always should return an array', () => {
+    const _input = [[32, 54, 123, 43], [42, 432]];
+    _input.forEach(myArray => {
+      expect(alternatingSums(myArray)).to.be.an('array');
+    });
+  });
 
   const _testArray = [
-    {input: [50, 60, 60, 45, 70], output: [180, 105]},
-    {input: [100, 50], output: [100, 50]},
-    {input: [100], output: [100, 0]},
-    {input: [], output: [0, 0]}
-  ]
+    { input: [50, 60, 60, 45, 70], output: [180, 105] },
+    { input: [100, 50], output: [100, 50] },
+    { input: [100], output: [100, 0] },
+    { input: [], output: [0, 0] }
+  ];
 
-  _testArray.forEach((object) => {
-    context(`for [${object.input}]`, () => {
-      it(`should return [${object.output}]`, () => {
-        expect(alternatingSums(object.input)).to.be.deep.equal(object.output);
-      })
+  _testArray.forEach(({input, output}) => {
+    it(`for [${input}] should return [${output}]`, () => {
+      expect(alternatingSums(input)).to.be.deep.equal(output);
     });
-  })
+  });
 });
