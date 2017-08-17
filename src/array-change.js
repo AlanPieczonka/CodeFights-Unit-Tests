6bcc417071e54fa9c8b114e3b2506962030a4bcc
Alan Pieczonka <alanpieczonka@gmail.com>
2017-08-18 00:52:58 +0200

    Changed let to const

--- src/array-change.js
+++ src/array-change.js
@@ -2,7 +2,7 @@ export default function(inputArray){
     let moves = 0;
     for(let i = 0; i < inputArray.length - 1; i++) {
         if(inputArray[i] >= inputArray[i+1]) {
-            let difference = inputArray[i]-inputArray[i+1]+1;
+            const difference = inputArray[i]-inputArray[i+1]+1;
             moves += difference;
             inputArray[i+1] = inputArray[i] + 1;
         }
